e386dbadf1c37ad539e392d67040749ba12038a8
Gulfide Caliskan <gulfide_535@hotmail.com>
2022-10-26 15:20:58 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace exception_handling
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+            /*
+            try
+            {
+                Console.WriteLine("Enter a number:");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The number you enter: " +num);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString()); 
+            }
+            */
+            /*
+            finally
+            {
+                Console.WriteLine("The process was completed.");
+            }
+            */
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("You entered a null value.");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Data type is not suitable.");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("You entered a very big value or a very small value.");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("The process was completed successfully.");
+            }
+
+         }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Patika_CSharp_ExceptionHandling
+### This project about try, catch, finally and logical errors.
+### You can visit [Patika.dev](https://www.patika.dev/tr) to see the task in C# Try, Catch, Finally ve Mantıksal Hatalar subject.
